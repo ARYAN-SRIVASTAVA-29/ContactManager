9515b1d65477382614849095a49dd9c5c5fbc470
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-04-08 22:07:39 +0530

    schema

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,8 +1,10 @@
 const asyncHandler = require("express-async-handler")
+const Contact = require("../models/contactModel");
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access public
 const getContacts = asyncHandler(async(req, res) => {
+    const contacts = await Contact.find();
     res.status(200).json({message: "Get all contacts"});
 })
 
@@ -16,13 +18,23 @@ const createContact = asyncHandler(async(req, res) => {
         res.status(400);
         throw new Error("All Fields are mandatory");
     }
-    res.status(201).json({message: "Create contact"});
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    })
+    res.status(201).json({contact});
 })
 
 //@desc Get Contact
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found");
+    }
     res.status(200).json({message: `Get Contact for ${req.params.id}`});
 })
 
